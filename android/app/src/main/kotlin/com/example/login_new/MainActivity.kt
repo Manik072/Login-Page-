c95a4ea59f7c6fafeb8795826b7d15b38ad3b2c2
Manik072 <manikgujral47@gmail.com>
2022-03-13 17:06:03 +0530

    Log in page

---
+++ android/app/src/main/kotlin/com/example/login_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.login_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'register.dart';
+import 'login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
